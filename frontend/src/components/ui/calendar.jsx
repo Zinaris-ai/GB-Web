7e5bf5ec8186cbaf7543fe8f81d32c104054be00
Dmitrij <Programmer.IQ@Gmail.com>
2025-10-21 11:21:00 +0300

    fix: добавлен кастомный форматтер для коротких названий дней недели + исправлена ширина ячеек календаря

--- frontend/src/components/ui/calendar.jsx
+++ frontend/src/components/ui/calendar.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 import { ru } from "date-fns/locale";
+import { format } from "date-fns";
 
 import { cn } from "../../lib/utils";
 import { buttonVariants } from "@/components/ui/button";
@@ -17,6 +18,9 @@ function Calendar({
   return (
     <DayPicker
       locale={ru}
+      formatters={{
+        formatWeekdayName: (date) => format(date, 'EEEEEE', { locale: ru })
+      }}
       showOutsideDays={showOutsideDays}
       mode={mode}
       onSelect={onSelect}
@@ -36,17 +40,17 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "text-muted-foreground flex-1 text-center rounded-md font-normal text-[0.8rem]",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: cn(
-          "flex-1 text-center text-sm p-0 relative focus-within:relative focus-within:z-20",
+          "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
           props.mode === "range"
             ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md"
             : "[&:has([aria-selected])]:rounded-md"
         ),
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-9 w-full p-0 font-normal aria-selected:opacity-100"
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
         day_range_start: "day-range-start",
         day_range_end: "day-range-end",
